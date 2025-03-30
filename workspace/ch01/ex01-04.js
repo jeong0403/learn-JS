cbcced6de9400dcd8bf61fd398da9028b8004522
jeong0403 <seehjl@gmail.com>
2025-03-30 20:14:45 +0900

    ✨ 백준 1단계 파일 코드 추가

---
+++ workspace/ch01/ex01-04.js
@@ -0,0 +1,10 @@
+/*
+명령문과 주석
+*/
+
+console.log("Hello, World!!!"); // 콘솔에 Hello, World! 출력
+console.log("Hello");
+console.log("World");
+
+console.log("Hello, World!!");
+console.log("Hello, World!!!");
---
+++ workspace/ch01/ex01-05.js
@@ -0,0 +1,42 @@
+/*
+변수 선언
+*/
+
+var a;
+var b, c;
+var d = 10;
+var e = 100, f = 200;
+
+var user = "하츄핑";
+var user2 = "아자핑";
+
+var messeage = '아자핑이 "안녕하세요." 라고 말했다.';
+var messeage = "아자핑이 '안녕하세요.' 라고 말했다.";
+var messeage = '피곤핑이 "피곤해"라고 말했다.'; // 이스케이프 문자(\)
+var messeage = "몰라핑이 \n모른다고 말\t했다.";
+var messeage = "하루핑아 \\놀자\\고 말했다.";
+
+var 변수1 = "좋아핑";
+var 이름_변수2 = "싫어핑";
+
+var $money = 4;
+var $4 = 4;
+
+var pi = 3.14;
+
+var _ = 200;
+var $ = 4;
+
+// var user-name = '안돼핑';
+var userName = '낙타핑';
+var userAge = 30;
+var userGender = "여자";
+var userEmail = "ping@gmail.com";
+
+console.log(user, messeage);
+
+console.log("ping@gmail.com");
+console.log(userEmail);
+
+userEmail = "ping@naver.com";
+console.log(userEmail);
