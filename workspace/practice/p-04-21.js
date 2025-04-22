2641bde154a2ebbfce51fedddf06dc46cda2ed44
jeong0403 <seehjl@gmail.com>
2025-04-22 22:18:49 +0900

    ✏️ practice: 0421

---
+++ workspace/practice/p-04-21.js
@@ -0,0 +1,14 @@
+// function printSum(x, y) {
+//   console.log(x + y);
+// }
+
+// const result = printSum(3, 4);
+// console.log("결과:", result);
+
+
+function add(x, y) {
+  return x + y;
+}
+
+const result = add(3, 4);
+console.log("결과:", result);
